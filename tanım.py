e7c5b1a20eefbd8f319557fc49c4e1c24e89d69f
iremferliler <49811925+iremferliler@users.noreply.github.com>
2019-05-23 23:48:03 +0300

    Create tanım.py

---
+++ tanım.py
@@ -0,0 +1,4 @@
+#Test verisetini pandas kütüphanesi ile tanımlıyoruz.
+df_test = pd.read_csv('../input/test.csv')
+#Eğitim verisetini pandas kütüphanesi ile tanımlıyoruz.
+df = pd.read_csv('../input/train.csv')
