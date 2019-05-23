838b735bc1d021e11ed6b929bec71e4287fef957
iremferliler <49811925+iremferliler@users.noreply.github.com>
2019-05-23 23:50:19 +0300

    Delete tanım.py

--- tanım.py
+++
@@ -1,4 +0,0 @@
-#Test verisetini pandas kütüphanesi ile tanımlıyoruz.
-df_test = pd.read_csv('../input/test.csv')
-#Eğitim verisetini pandas kütüphanesi ile tanımlıyoruz.
-df = pd.read_csv('../input/train.csv')
