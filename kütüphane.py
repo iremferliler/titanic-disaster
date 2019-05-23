6874de4cf1dba1a973b16a2faabc1c2faf5c80ed
iremferliler <49811925+iremferliler@users.noreply.github.com>
2019-05-23 23:44:46 +0300

    Create kütüphane.py

---
+++ kütüphane.py
@@ -0,0 +1,8 @@
+#Python 3 ortamı, yüklü birçok faydalı analitik kütüphanesiyle birlikte gelir.
+import time
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from keras import models
+from keras.layers import Dense, Dropout
+from sklearn.preprocessing import MinMaxScaler
